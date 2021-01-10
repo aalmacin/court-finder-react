3213b20048e8652d1fd0a80cdf9f87e638be8aaf
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-01-10 14:40:57 -0500

    Setup dev server

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,8 @@ module.exports = {
         rules: [
             { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
     }
 };
