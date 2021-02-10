7861550f77e6f63de379a316c2aedb119a58d001
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-02-10 18:19:30 -0500

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve('dist')
-    },
-    // define babel loader
-    module: {
-        rules: [
-            { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist')
-    }
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve("dist"),
+  },
+  // define babel loader
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
 };
